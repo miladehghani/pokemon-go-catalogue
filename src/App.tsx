35b5049cc4b4de8f74b10d672b2a7873bf8add83
Milad <Milad.1372.g@gmail.com>
2021-06-25 23:07:39 +0430

    setup react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import { UserRoutes } from "routes";
 import { AppHeader as Header } from "components/AppHeader";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 export const App = () => (
   <BrowserRouter>
     <Header />
-    <UserRoutes />
+    <QueryClientProvider client={queryClient}>
+      <UserRoutes />
+    </QueryClientProvider>
   </BrowserRouter>
 );
