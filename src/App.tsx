1c8c4ae5e609bd703b9c7e56c0d3499c98ed6d80
Milad <Milad.1372.g@gmail.com>
2021-06-27 13:23:02 +0430

    setup redux presistor

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,21 @@ import { AppHeader as Header } from "components/AppHeader";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import { store } from "store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 const queryClient = new QueryClient();
+let persistor = persistStore(store);
 
 export const App = () => (
   <Provider store={store}>
-    <BrowserRouter>
-      <Header />
-      <QueryClientProvider client={queryClient}>
-        <UserRoutes />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Header />
+        <QueryClientProvider client={queryClient}>
+          <UserRoutes />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
 import { favoriteReducer } from "./slices/favorites";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
+const persistConfig = {
+  key: "store-v1",
+  storage,
+};
+
+const reducer = combineReducers({
+  favorites: favoriteReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  reducer: {
-    favorites: favoriteReducer,
-  },
+  devTools: process.env.NODE_ENV !== "production",
+  reducer: persistedReducer,
 });
 
 export type AppDispatch = typeof store.dispatch;
