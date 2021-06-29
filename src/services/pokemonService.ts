4474724e9e220d5697c0a68c7b27897bd8dec453
Milad <Milad.1372.g@gmail.com>
2021-06-29 18:04:43 +0430

    extract pokemon baseUrl into config file

--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -1,11 +1,12 @@
+import { config } from "config";
 import { Generation } from "types/generation";
 import { Pokemon, Resource } from "types/pokemon";
 import { PaginatedResponse } from "types/pokemonApi";
 import { Species } from "types/Species";
 
-const baseUrl = "https://pokeapi.co/api/v2";
-
 class PokemonService {
+  baseUrl = config.PokemonApiUrl;
+
   fetch = async (url: string) => {
     const response = await fetch(url);
     if (!response.ok) throw Error(await response.text());
@@ -13,19 +14,19 @@ class PokemonService {
   };
 
   fetchAllPokemon = async (): Promise<PaginatedResponse<Resource[]>> =>
-    this.fetch(`${baseUrl}/pokemon?limit=2000`);
+    this.fetch(`${this.baseUrl}/pokemon?limit=2000`);
 
   getPokemonById = async (id: number): Promise<Pokemon> =>
-    this.fetch(`${baseUrl}/pokemon/${id}`);
+    this.fetch(`${this.baseUrl}/pokemon/${id}`);
 
   getGenerations = async (): Promise<PaginatedResponse<Resource[]>> =>
-    this.fetch(`${baseUrl}/generation`);
+    this.fetch(`${this.baseUrl}/generation`);
 
   getGenerationById = async (id: number): Promise<Generation> =>
-    this.fetch(`${baseUrl}/generation/${id}`);
+    this.fetch(`${this.baseUrl}/generation/${id}`);
 
   getSpeciesById = (id: number): Promise<Species> =>
-    this.fetch(`${baseUrl}/pokemon-species/${id}/`);
+    this.fetch(`${this.baseUrl}/pokemon-species/${id}/`);
 }
 
 export { PokemonService };
